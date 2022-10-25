78a6ca18feb52fd06f2b1a249f5498a5f99d57a8
samuelcpr <samuelcpr464@gmail.com>
2022-10-25 08:33:48 -0300

    feat:add file index project

--- Hash/index01.rb
+++ Hash/index01.rb
@@ -8,4 +8,15 @@ irb(main):008:0> capitais[:minas_gerais] = "Belo Horizonte" # Adiciona valor e c
 irb(main):009:0> capitais
 => {:acre=>"Rio Branco", :são_paulo=>"São Paulo", :minas_gerais=>"Belo Horizonte"}
 irb(main):010:0> 
+
+irb(main):010:0> capitais.delete(:acre) #deleção de chave
+=> "Rio Branco"
+irb(main):011:0> capitais
+=> {:são_paulo=>"São Paulo", :minas_gerais=>"Belo Horizonte"}
+irb(main):012:0> 
                
+# retorna verdadeiro ou falso se contiver dados retorna false se estiver vasio 
+# retorna true
+irb(main):013:0> capitais.empty?
+=> false
+irb(main):014:0> 
