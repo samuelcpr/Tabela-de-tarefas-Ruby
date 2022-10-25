ab56dbb269505b9c79812a9823802df9197db465
samuelcpr <samuelcpr464@gmail.com>
2022-10-25 08:33:32 -0300

    feat: add file each

---
+++ each_array/each_array.rb
@@ -0,0 +1,20 @@
+names = ['joãozinho', 'Manoel', 'juca']
+
+name = 'Leonardo Rocha'
+
+names.each do |name|
+    puts name
+end
+
+puts name
+
+=begin
+
+samuel@samuel-Lenovo-IdeaPad-S145-15IIL:~/Documentos/Tabela-de-tarefas-Ruby/each_array$ ruby each_array.rb 
+joãozinho
+Manoel
+juca
+Leonardo Rocha
+
+
+=end
