9599f450acdd4976b85356e6770936f025ff4248
samuelcpr <samuelcpr464@gmail.com>
2022-11-05 14:14:26 -0300

    feat: create function

---
+++ each_array/select_array.rb
@@ -0,0 +1,12 @@
+array = [1,2,3,4,5,6]
+
+selection = array.select do |element|
+    element >= 4
+end
+
+puts selection
+
+=begin
+
+selection = selecione todos os elementos do array que forem menor ou = 4
+=end
